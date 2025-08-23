2655a86ecf7562a11d2341f38a112161f0541acc
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-08-23 16:52:59 -0300

    time all operations

--- main.go
+++ main.go
@@ -13,20 +13,35 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Read set list. Got %d sets.", len(setData.Data))
+	setReadTime := time.Now()
+	log.Printf(
+		"Read set list. Got %d sets in %.2f seconds.",
+		len(setData.Data),
+		setReadTime.Sub(start).Seconds(),
+	)
 
 	cardData, err := ReadAtomicCards()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Read all atomic cards. Got %d cards.", len(cardData.Data))
+	atomicReadTime := time.Now()
+	log.Printf(
+		"Read all atomic cards. Got %d cards in %.2f seconds.",
+		len(cardData.Data),
+		atomicReadTime.Sub(setReadTime).Seconds(),
+	)
 
 	setCardData, err := ReadSetCards()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Read all set cards. Got %d cards versions.", len(setCardData.Data))
+	setCardReadTime := time.Now()
+	log.Printf(
+		"Read all set cards. Got %d cards versions in %.2f seconds.",
+		len(setCardData.Data),
+		setCardReadTime.Sub(atomicReadTime).Seconds(),
+	)
 
 	end := time.Now()
-	log.Printf("Took %.2f seconds.", end.Sub(start).Seconds())
+	log.Printf("Took %.2f seconds to read sets, cards, and set-cards", end.Sub(start).Seconds())
 }
