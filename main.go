c4bac6ab40a0261eb8336994b1aca09b978e8fe8
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-08-23 16:42:57 -0300

    read all files in main.go and print totals

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
 )
 
 func main() {
 
-	data, err := ReadSetCards()
+	setData, err := ReadSetList()
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Read set list. Got %d sets.\n", len(setData.Data))
 
-	fmt.Printf("%+v", data.Data[:5])
+	cardData, err := ReadAtomicCards()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Read all atomic cards. Got %d cards.", len(cardData.Data))
+
+	setCardData, err := ReadSetCards()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Read all set cards. Got %d cards versions.", len(setCardData.Data))
 }
