61130fd917da9119ad4e47007a037e5ce668759f
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-08-24 01:14:55 -0300

    add layout and method to check if equal to db card

--- internal/datamodel/cards.go
+++ internal/datamodel/cards.go
@@ -1,9 +1,14 @@
 package datamodel
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+
+	"FedeAbella/mtgdb/internal/sqlc"
+)
 
 type AtomicCard struct {
 	Name        string      `json:"name"`
+	Layout      string      `json:"layout"`
 	Identifiers Identifiers `json:"identifiers"`
 }
 
@@ -13,4 +18,8 @@ type SetCard struct {
 	Name        string      `json:"name"`
 	Set         string      `json:"setCode"`
 	NumberInSet string      `json:"number"`
+}
+
+func (c *AtomicCard) Equals(dbCard *sqlc.Card) bool {
+	return c.Name == dbCard.Name && c.Identifiers.ScryfallOracleId == dbCard.ScryfallOracleID.Bytes
 }
