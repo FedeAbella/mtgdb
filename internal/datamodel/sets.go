8d3171e740b2713d5352cd0cdfe0922ee1704883
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-08-23 21:25:43 -0300

    add equality check between file and db set

--- internal/datamodel/sets.go
+++ internal/datamodel/sets.go
@@ -1,6 +1,14 @@
 package datamodel
 
+import (
+	"FedeAbella/mtgdb/internal/sqlc"
+)
+
 type Set struct {
 	Code string `json:"code"`
 	Name string `json:"name"`
+}
+
+func (s *Set) Equals(dbSet sqlc.Set) bool {
+	return s.Code == dbSet.Code && s.Name == dbSet.Name
 }
