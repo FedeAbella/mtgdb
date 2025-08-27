1634626ebfccb76dcd025df910bf8edebbb99440
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-08-26 21:45:25 -0300

    update schema

--- sql/schema/002_cards.sql
+++ sql/schema/002_cards.sql
@@ -1,8 +1,18 @@
 -- +goose Up
 CREATE TABLE cards (
-    id UUID PRIMARY KEY,
-    scryfall_oracle_id UUID NOT NULL UNIQUE,
-    name TEXT NOT NULL UNIQUE,
+    scryfall_id UUID PRIMARY KEY,
+    set_id UUID NOT NULL REFERENCES sets(scryfall_id) ON DELETE CASCADE ,
+    name TEXT NOT NULL,
+    collector_number TEXT NOT NULL,
+    color_identity TEXT,
+    colors TEXT,
+    language_code TEXT NOT NULL,
+    spanish_name TEXT,
+    rarity TEXT,
+    type_line TEXT,
+    scryfall_api_uri TEXT NOT NULL UNIQUE,
+    scryfall_web_uri TEXT NOT NULL UNIQUE,
+    scryfall_oracle_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
